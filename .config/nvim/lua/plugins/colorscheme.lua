ff190d3657268fab7a5d2af235ef2c8ab344771e
Pierre-Louis Renaudin <pl.renaudin@gmail.com>
2023-12-16 14:32:16 +0000

    change colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,14 @@
 return {
-  {
-    "olimorris/onedarkpro.nvim",
-    lazy = true,
-    opts = { style = "night" },
-  },
+  -- {
+  --   "olimorris/onedarkpro.nvim",
+  --   lazy = true,
+  --   opts = { style = "night" },
+  -- },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onedark",
+      colorscheme = "catppuccin",
     },
   },
 }
