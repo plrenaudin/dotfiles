8c60ca703f46ed5da0bda1e3b7e7b1b5761d115b
Pierre-Louis Renaudin <pl.renaudin@gmail.com>
2024-02-06 21:34:21 +0000

    disable leap

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -2,4 +2,7 @@ return {
   -- disable trouble
   -- { "folke/trouble.nvim", enabled = false },
   { "echasnovski/mini.pairs", enabled = false },
+  { "ggandor/leap.nvim", enabled = false },
+  { "ggandor/flit.nvim", enabled = false },
+  { "folke/flash.nvim", enabled = false },
 }
