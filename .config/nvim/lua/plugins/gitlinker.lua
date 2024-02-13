8f1d992252c9a32134371016ca7044ec25cb6480
Pierre-Louis Renaudin <pl.renaudin@gmail.com>
2024-02-13 12:47:11 +0000

    Add gitlinker extension

---
+++ .config/nvim/lua/plugins/gitlinker.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "ruifm/gitlinker.nvim",
+    event = "VeryLazy",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      {
+        "<leader>gy",
+        "<cmd>lua require('gitlinker').get_buf_range_url('n')<cr>",
+        desc = "Copy Git Link",
+      },
+      {
+        "<leader>gY",
+        "<cmd>lua require('gitlinker').get_buf_range_url('n', {action_callback = require('gitlinker.actions').open_in_browser})<cr>",
+        desc = "Open Git Link",
+      },
+    },
+    opts = {},
+  },
+}
