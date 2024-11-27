9d06e898b614ee84bcdf9d10652d1ff851a38ab4
Pierre-Louis Renaudin <pl.renaudin@gmail.com>
2024-11-27 15:31:05 +0000

    move to code companion

--- .config/nvim/lua/plugins/ollama.lua
+++ .config/nvim/lua/plugins/ollama.lua
@@ -1,33 +1,83 @@
+-- return {
+--   "nomnivore/ollama.nvim",
+--   dependencies = {
+--     "nvim-lua/plenary.nvim",
+--   },
+--
+--   -- All the user commands added by the plugin
+--   cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
+--
+--   keys = {
+--     -- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
+--     {
+--       "<leader>oo",
+--       ":<c-u>lua require('ollama').prompt()<cr>",
+--       desc = "ollama prompt",
+--       mode = { "n", "v" },
+--     },
+--
+--     -- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
+--     {
+--       "<leader>oG",
+--       ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
+--       desc = "ollama Generate Code",
+--       mode = { "n", "v" },
+--     },
+--   },
+--
+--   ---@type Ollama.Config
+--   opts = {
+--     -- your configuration overrides
+--     model = "qwen2.5-coder:14b",
+--   },
+-- }
+--
 return {
-  "nomnivore/ollama.nvim",
+  "olimorris/codecompanion.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "hrsh7th/nvim-cmp", -- Optional: For using slash commands and variables in the chat buffer
+    "nvim-telescope/telescope.nvim", -- Optional: For using slash commands
   },
-
-  -- All the user commands added by the plugin
-  cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
-
+  config = true,
+  opts = {
+    strategies = {
+      chat = { adapter = "ollama" },
+      inline = { adapter = "ollama" },
+      agent = { adapter = "ollama" },
+    },
+    adapters = {
+      ollama = function()
+        return require("codecompanion.adapters").extend("ollama", {
+          name = "qwen2.5-coder:14b",
+          schema = {
+            model = {
+              default = "qwen2.5-coder:14b",
+            },
+            num_ctx = {
+              default = 16384,
+            },
+            num_predict = {
+              default = -1,
+            },
+          },
+        })
+      end,
+    },
+  },
   keys = {
-    -- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
     {
       "<leader>oo",
-      ":<c-u>lua require('ollama').prompt()<cr>",
-      desc = "ollama prompt",
+      "<cmd>CodeCompanionActions<cr>",
+      desc = "Ollama Code Companion",
       mode = { "n", "v" },
     },
-
-    -- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
     {
-      "<leader>oG",
-      ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
-      desc = "ollama Generate Code",
+      "<leader>oc",
+      "<cmd>CodeCompanion<cr>",
+      desc = "Ollama Inline Companion",
       mode = { "n", "v" },
     },
-  },
-
-  ---@type Ollama.Config
-  opts = {
-    -- your configuration overrides
-    model = "qwen2.5-coder:14b",
   },
 }
