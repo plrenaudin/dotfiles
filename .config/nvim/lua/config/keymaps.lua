d601106a6b73b18238c764296a3d5fa41527fa49
Pierre-Louis Renaudin <pl.renaudin@gmail.com>
2024-11-29 16:39:28 +0000

    Allow file history browsing with telescope

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>gF", function()
+  local current_file_dir = vim.fn.fnamemodify(vim.fn.expand("%:p"), ":h")
+  local git_root = vim.fn.systemlist("git -C " .. current_file_dir .. " rev-parse --show-toplevel")[1]
+  vim.notify(git_root, vim.log.levels.ERROR)
+
+  if vim.v.shell_error == 0 then
+    require("telescope.builtin").git_bcommits({
+      cwd = git_root, -- Set the working directory to the Git root
+    })
+  else
+    vim.notify("Not in a Git repository!", vim.log.levels.ERROR)
+  end
+end, { desc = "Git File History" })
