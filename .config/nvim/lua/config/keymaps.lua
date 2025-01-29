55a01ca341b2d44adcf5864e92ab078d7a907adf
Pierre-Louis Renaudin <pl.renaudin@gmail.com>
2025-01-29 10:26:07 +0000

    replace fzf-lua by snacks picker

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,35 +2,35 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local gitsigns = require("gitsigns")
-local fzf = require("fzf-lua")
+-- local fzf = require("fzf-lua")
 
-vim.keymap.set("n", "<leader>gF", function()
-  fzf.git_bcommits()
-end, { desc = "Git File History" })
+-- vim.keymap.set("n", "<leader>gF", function()
+--   fzf.git_bcommits()
+-- end, { desc = "Git File History" })
 
-vim.keymap.set("n", "<leader>go", function()
-  local current_file_dir = vim.fn.fnamemodify(vim.fn.expand("%:p"), ":h")
-  local git_root = vim.fn.systemlist("git -C " .. current_file_dir .. " rev-parse --show-toplevel")[1]
-  fzf.git_branches({
-    cwd = git_root,
-    actions = {
-      ["default"] = function(selected_branch)
-        -- Extract branch name from the table
-        local branch_name = vim.fn.trim(selected_branch[1]) -- Trim any extra whitespace
-        if not branch_name or branch_name == "" then
-          vim.notify("No branch selected", vim.log.levels.ERROR)
-          return
-        end
-
-        -- Get the Git root directory
-        if not git_root or git_root == "" then
-          vim.notify("Not in a Git repository", vim.log.levels.ERROR)
-          return
-        end
-
-        -- Diff the file using gitsigns
-        gitsigns.diffthis(branch_name)
-      end,
-    },
-  })
-end, { desc = "Diff current file with selected branch" })
+-- vim.keymap.set("n", "<leader>go", function()
+--   local current_file_dir = vim.fn.fnamemodify(vim.fn.expand("%:p"), ":h")
+--   local git_root = vim.fn.systemlist("git -C " .. current_file_dir .. " rev-parse --show-toplevel")[1]
+--   fzf.git_branches({
+--     cwd = git_root,
+--     actions = {
+--       ["default"] = function(selected_branch)
+--         -- Extract branch name from the table
+--         local branch_name = vim.fn.trim(selected_branch[1]) -- Trim any extra whitespace
+--         if not branch_name or branch_name == "" then
+--           vim.notify("No branch selected", vim.log.levels.ERROR)
+--           return
+--         end
+--
+--         -- Get the Git root directory
+--         if not git_root or git_root == "" then
+--           vim.notify("Not in a Git repository", vim.log.levels.ERROR)
+--           return
+--         end
+--
+--         -- Diff the file using gitsigns
+--         gitsigns.diffthis(branch_name)
+--       end,
+--     },
+--   })
+-- end, { desc = "Diff current file with selected branch" })
