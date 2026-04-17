41443a5aff99419fd458f78cb21cf4474dbe6c10
Pierre-Louis Renaudin <pl.renaudin@gmail.com>
2026-04-17 11:49:56 +0100

    fix open file in branch

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -5,27 +5,32 @@
 vim.api.nvim_set_keymap("n", "M-j", ":cnext<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "M-k", ":cprevious<CR>", { noremap = true, silent = true })
 
-local snacks = require("snacks")
-local gitsigns = require("gitsigns")
-
-local function display_diff(picker, item)
-  picker:close()
-  if item then
-    gitsigns.diffthis(item.branch)
-  end
-end
-
--- Custom function to show diff with selected branch using snacks picker
 vim.keymap.set("n", "<leader>go", function()
-  snacks.picker.git_branches({
+  local bufnr = vim.api.nvim_get_current_buf()
+  local file = vim.api.nvim_buf_get_name(bufnr)
+  local cwd = file ~= "" and vim.fn.fnamemodify(file, ":h") or vim.fn.getcwd()
+  require("snacks").picker.git_branches({
     finder = "git_branches",
     layout = "select",
     format = "git_branch",
-    current_file = true,
-    follow = true,
-    confirm = display_diff,
+    cwd = cwd,
+    confirm = function(picker, item)
+      picker:close()
+      if not item then
+        return
+      end
+      local ref = item.branch or item.commit
+      if not ref then
+        return
+      end
+      vim.api.nvim_buf_call(bufnr, function()
+        require("gitsigns").diffthis(ref)
+      end)
+    end,
   })
 end, { desc = "Diff current file with selected branch" })
 
 vim.keymap.set("n", "<leader>ff", LazyVim.pick("files", { root = false }))
 vim.keymap.set("n", "<leader>sg", LazyVim.pick("live_grep", { root = false }))
+
+vim.keymap.set("n", "<leader>R", "<cmd>source $MYVIMRC<CR>", { desc = "Reload config" })
